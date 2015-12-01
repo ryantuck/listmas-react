551402814351b922377a19988b3e73679d59a147
Ryan Tuck <ryntck@gmail.com>
2015-11-30 22:48:32 -0500

    test front end changes to data model

--- static/build/example2.js
+++ static/build/example2.js
@@ -3,13 +3,57 @@
 
 var ListItem = React.createClass({
     displayName: 'ListItem',
+    changeColor: function() {
+            this.setState({
+                count: this.state.count + 1
+            });
+            if (this.state.background == 'green') {
+                this.setState({
+                    background: 'red'
+                });
+            }
+            else {
+                this.setState({
+                    background: 'green'
+                });
+            }
+            console.log('background changed!');
+            console.log(this.state.background);
+            console.log(this.state.count);
+
+    },
+    getInitialState: function() {
+        return {
+            background: 'red',
+            count: 0
+        }
+    },
     render: function() {
         return (
                 React.createElement('div',
-                    {className: 'list-item'},
+                    {
+                        className: 'list-item',
+                        style: { backgroundColor: this.state.background }
+                    },
                     React.createElement('h1',
-                        {className: 'list-item-title'},
-                        'list item title'
+                        {
+                            className: 'list-item-title'
+                        },
+                        this.state.count
+                        ),
+                    React.createElement('p',
+                        {
+                            className: 'list-item-notes',
+                            style: { color: 'yellow' }
+                        },
+                        'list item notes'
+                        ),
+                    React.createElement('button',
+                        {
+                            className: 'list-item-button',
+                            onClick: this.changeColor
+                        },
+                        'claim'
                         )
                     )
                );
