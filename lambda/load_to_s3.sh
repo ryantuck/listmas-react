02069fc3a63c8b767e1f54312e0260fd95dc0745
Ryan Tuck <ryntck@gmail.com>
2015-12-03 21:41:06 -0500

    clean up zip creation

--- lambda/load_to_s3.sh
+++ lambda/load_to_s3.sh
@@ -1,4 +1,4 @@
-zip users.zip users.py;
-s3cmd put users.zip s3://ryntck-lambda-test;
+zip user.zip user.py;
+s3cmd put user.zip s3://ryntck-lambda-test;
 zip present.zip present.py;
 s3cmd put present.zip s3://ryntck-lambda-test;
